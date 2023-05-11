6fd446f9071ebc24886fa65c4b3df281d732c73b
Phymas <garylau@live.com>
2023-05-11 11:07:20 +0800

    Fix: Wrong form for registration page

--- src/components/Authentication/Authentication.tsx
+++ src/components/Authentication/Authentication.tsx
@@ -1,9 +1,6 @@
 import { useState } from "react";
 import { useForm } from "@mantine/form";
-import {
-	Paper,
-	PaperProps,
-} from "@mantine/core";
+import { Paper, PaperProps } from "@mantine/core";
 import { signIn } from "next-auth/react";
 import { useMutation } from "@apollo/client";
 import { ADD_USER } from "@operations/mutations";
@@ -11,103 +8,104 @@ import { EMAIL_PATTERN, PASSWORD_PATTERN } from "@constants/regex";
 import AuthenticationScreen from "./AuthenticationScreen";
 import ResetPasswordScreen from "./ResetPasswordScreen";
 
-type AuthScreens = 'login' | 'register' | 'forgotPassword';
+type AuthScreens = "login" | "register" | "forgotPassword";
 
 interface AuthProps extends PaperProps {
-	isModal?: boolean;
-	defaultPage?: string;
+  isModal?: boolean;
+  defaultPage?: AuthScreens;
 }
 
-const Authentication = ({ isModal = false, defaultPage = "login", ...props }: AuthProps) => {
-	const [screen, setScreen] = useState<AuthScreens>('login');
-	const [error, setError] = useState("");
-	const [register] = useMutation(ADD_USER);
+const Authentication = ({
+  isModal = false,
+  defaultPage = "login",
+  ...props
+}: AuthProps) => {
+  const [screen, setScreen] = useState<AuthScreens>(defaultPage);
+  const [error, setError] = useState("");
+  const [register] = useMutation(ADD_USER);
 
-	const form = useForm({
-		initialValues: {
-			name: "",
-			email: "",
-			password: "",
-			terms: true,
-		},
+  const form = useForm({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+      terms: true,
+    },
 
-		validate: {
-			name: (value) =>
-				(value.trim().length < 3 || value.trim().length > 190) &&
-				"Invalid username",
-			email: (val) => !EMAIL_PATTERN.test(val) && "Invalid email",
-			password: (val) =>
-				!PASSWORD_PATTERN.test(val) &&
-				"Password should include at least 6 characters, 1 uppercase letter, 1 lowercase letter, 1 number and 1 special character",
-		},
-	});
+    validate: {
+      name: (value) =>
+        (value.trim().length < 3 || value.trim().length > 190) &&
+        "Invalid username",
+      email: (val) => !EMAIL_PATTERN.test(val) && "Invalid email",
+      password: (val) =>
+        !PASSWORD_PATTERN.test(val) &&
+        "Password should include at least 6 characters, 1 uppercase letter, 1 lowercase letter, 1 number and 1 special character",
+    },
+  });
 
-	const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		const { hasErrors, errors } = form.validate();
+  const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { hasErrors, errors } = form.validate();
 
-		if (screen === "login") {
-			// Ignore username validation
-			if (hasErrors && (errors.email || errors.password)) {
-				return;
-			}
+    if (screen === "login") {
+      // Ignore username validation
+      if (hasErrors && (errors.email || errors.password)) {
+        return;
+      }
 
-			// Perform login with graphql query
-			signIn("credentials", {
-				email: form.values.email,
-				password: form.values.password,
-				callbackUrl: "/",
-				// redirect: false,
-			});
-		} else if (screen === "register") {
-			if (hasErrors) {
-				return;
-			}
+      // Perform login with graphql query
+      signIn("credentials", {
+        email: form.values.email,
+        password: form.values.password,
+        callbackUrl: "/",
+        // redirect: false,
+      });
+    } else if (screen === "register") {
+      if (hasErrors) {
+        return;
+      }
 
-			const response = await register({
-				variables: {
-					name: form.values.name,
-					email: form.values.email,
-					password: form.values.password,
-				},
-			});
+      const response = await register({
+        variables: {
+          name: form.values.name,
+          email: form.values.email,
+          password: form.values.password,
+        },
+      });
 
-			if (response.data.addUser.code === 200) {
-				signIn("credentials", {
-					email: form.values.email,
-					password: form.values.password,
-					callbackUrl: "/",
-					// redirect: false,
-				});
-			} else {
-				setError(response.data.addUser.code);
-				form.setErrors({ email: "Email has been registered" });
-			}
-		}
-	};
+      if (response.data.addUser.code === 200) {
+        signIn("credentials", {
+          email: form.values.email,
+          password: form.values.password,
+          callbackUrl: "/",
+          // redirect: false,
+        });
+      } else {
+        setError(response.data.addUser.code);
+        form.setErrors({ email: "Email has been registered" });
+      }
+    }
+  };
 
-	const resetPasswordScreen = (
-		<ResetPasswordScreen form={form} setScreen={setScreen} />
-	);
+  const resetPasswordScreen = (
+    <ResetPasswordScreen form={form} setScreen={setScreen} />
+  );
 
-	const authenticationScreen = (
-		<AuthenticationScreen
-			form={form}
-			screen={screen}
-			setScreen={setScreen}
-			submitHandler={submitHandler}
-			error={error}
-		/>
-	);
+  const authenticationScreen = (
+    <AuthenticationScreen
+      form={form}
+      screen={screen}
+      setScreen={setScreen}
+      submitHandler={submitHandler}
+      error={error}
+    />
+  );
 
-	return (
-		<Paper radius="md" p="xl" withBorder {...props}>
-			{screen === "forgotPassword"
-				? resetPasswordScreen
-				: authenticationScreen}
-		</Paper>
-	);
+  return (
+    <Paper radius="md" p="xl" withBorder {...props}>
+      {screen === "forgotPassword" ? resetPasswordScreen : authenticationScreen}
+    </Paper>
+  );
 };
 
 export default Authentication;
-
