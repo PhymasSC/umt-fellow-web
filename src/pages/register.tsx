542fdf4fd4760e67915b41d76f318553a05ad742
Phymas <garylau@live.com>
2023-05-11 11:07:20 +0800

    Fix: Wrong form for registration page

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,70 +1,70 @@
-import { Paper, createStyles } from "@mantine/core";
+import { Paper, createStyles, Alert } from "@mantine/core";
 import { Authentication } from "@components/index";
 import { NextPage } from "next";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 const useStyles = createStyles((theme) => ({
-	wrapper: {
-		display: "flex",
-		alignItems: "center",
-		height: "100vh",
-		padding: "3em",
-		backgroundSize: "cover",
-		backgroundImage:
-			"url(https://images.unsplash.com/photo-1633155518197-afebeecd096d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=769&q=80)",
-		[`@media (max-width: ${theme.breakpoints.sm}px)`]: {
-			padding: "0",
-		},
-	},
+  wrapper: {
+    display: "flex",
+    alignItems: "center",
+    height: "100vh",
+    padding: "3em",
+    backgroundSize: "cover",
+    backgroundImage:
+      "url(https://images.unsplash.com/photo-1633155518197-afebeecd096d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=769&q=80)",
+    [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
+      padding: "0",
+    },
+  },
 
-	form: {
-		display: "flex",
-		flexDirection: "column",
-		justifyContent: "center",
-		border: `1px solid ${
-			theme.colorScheme === "dark"
-				? theme.colors.dark[7]
-				: theme.colors.gray[3]
-		}`,
-		minWidth: "450px",
-		width: "10em",
-		paddingTop: 80,
-		borderRadius: ".5em",
-		[`@media (max-width: ${theme.breakpoints.sm}px)`]: {
-			maxWidth: "100%",
-			borderRadius: "0",
-			height: "100vh",
-		},
-	},
+  form: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    border: `1px solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.colors.gray[3]
+    }`,
+    minWidth: "450px",
+    width: "10em",
+    paddingTop: 80,
+    borderRadius: ".5em",
+    [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
+      maxWidth: "100%",
+      borderRadius: "0",
+      height: "100vh",
+    },
+  },
 
-	title: {
-		color: theme.colorScheme === "dark" ? theme.white : theme.black,
-		fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-	},
+  title: {
+    color: theme.colorScheme === "dark" ? theme.white : theme.black,
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+  },
 
-	logo: {
-		color: theme.colorScheme === "dark" ? theme.white : theme.black,
-		width: 120,
-		display: "block",
-		marginLeft: "auto",
-		marginRight: "auto",
-	},
+  logo: {
+    color: theme.colorScheme === "dark" ? theme.white : theme.black,
+    width: 120,
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
 }));
 
 const Register: NextPage = () => {
-	const { classes } = useStyles();
-	return (
-		<>
-			<Head>
-				<title>Register | UMT Fellow</title>
-			</Head>
-			<div className={classes.wrapper}>
-				<Paper className={classes.form} radius={0} p={30}>
-					<Authentication defaultPage="register" />
-				</Paper>
-			</div>
-		</>
-	);
+  const router = useRouter();
+  const { classes } = useStyles();
+  return (
+    <>
+      <Head>
+        <title>Register | UMT Fellow</title>
+      </Head>
+      <div className={classes.wrapper}>
+        <Paper className={classes.form} radius={0} p={30}>
+          <Authentication defaultPage="register" />
+        </Paper>
+      </div>
+    </>
+  );
 };
 
 export default Register;
