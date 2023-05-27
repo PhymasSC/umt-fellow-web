889b55c439713f96ce4a84c33867461002ad2c1f
Phymas <garylau@live.com>
2023-05-27 14:29:35 +0800

    UI: Update contact us ui

--- src/pages/contact-us.tsx
+++ src/pages/contact-us.tsx
@@ -1,165 +1,142 @@
 import {
-	createStyles,
-	Text,
-	Title,
-	SimpleGrid,
-	TextInput,
-	Textarea,
-	Button,
-	Group,
-	ActionIcon,
-	Container,
-	Stack,
+  createStyles,
+  Text,
+  Title,
+  SimpleGrid,
+  TextInput,
+  Textarea,
+  Button,
+  Group,
+  ActionIcon,
+  Container,
+  Stack,
 } from "@mantine/core";
 import {
-	IconBrandTwitter,
-	IconBrandYoutube,
-	IconBrandInstagram,
+  IconBrandTwitter,
+  IconBrandYoutube,
+  IconBrandInstagram,
 } from "@tabler/icons";
 import { Footer } from "@components/index";
 import { NextPage } from "next";
 
 const useStyles = createStyles((theme) => ({
-	wrapper: {
-		minHeight: 400,
-		boxSizing: "border-box",
-		backgroundImage: `linear-gradient(-60deg, ${theme.colors[theme.primaryColor][4]
-			} 0%, ${theme.colors[theme.primaryColor][7]} 100%)`,
-		borderRadius: theme.radius.md,
-		padding: `calc(${theme.spacing.xl}) * 2.5`,
-		margin: `calc(${theme.spacing.xl}) * 2.5`,
-		[`@media (max-width: ${theme.breakpoints.sm}px)`]: {
-			padding: `calc(${theme.spacing.xl}) * 1.5`,
-		},
-	},
+  wrapper: {
+    minHeight: 400,
+    boxSizing: "border-box",
+    backgroundImage: `linear-gradient(-60deg, ${
+      theme.colors[theme.primaryColor][4]
+    } 0%, ${theme.colors[theme.primaryColor][7]} 100%)`,
+    borderRadius: theme.radius.md,
+    padding: "3em",
+  },
 
-	title: {
-		fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-		color: theme.white,
-		lineHeight: 1,
-	},
+  title: {
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    color: theme.white,
+    lineHeight: 1,
+  },
 
-	description: {
-		color: theme.colors[theme.primaryColor][0],
-		maxWidth: 300,
+  description: {
+    color: theme.colors[theme.primaryColor][0],
+    maxWidth: 300,
 
-		[`@media (max-width: ${theme.breakpoints.sm}px)`]: {
-			maxWidth: "100%",
-		},
-	},
+    [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
+      maxWidth: "100%",
+    },
+  },
 
-	form: {
-		backgroundColor: theme.white,
-		padding: theme.spacing.xl,
-		borderRadius: theme.radius.md,
-		boxShadow: theme.shadows.lg,
-	},
+  form: {
+    backgroundColor: theme.white,
+    padding: theme.spacing.xl,
+    borderRadius: theme.radius.md,
+    boxShadow: theme.shadows.lg,
+  },
 
-	social: {
-		color: theme.white,
+  social: {
+    color: theme.white,
 
-		"&:hover": {
-			color: theme.colors[theme.primaryColor][1],
-		},
-	},
+    "&:hover": {
+      color: theme.colors[theme.primaryColor][1],
+    },
+  },
 
-	input: {
-		backgroundColor: theme.white,
-		borderColor: theme.colors.gray[4],
-		color: theme.black,
+  input: {
+    backgroundColor: theme.white,
+    borderColor: theme.colors.gray[4],
+    color: theme.black,
 
-		"&::placeholder": {
-			color: theme.colors.gray[5],
-		},
-	},
+    "&::placeholder": {
+      color: theme.colors.gray[5],
+    },
+  },
 
-	inputLabel: {
-		color: theme.black,
-	},
+  inputLabel: {
+    color: theme.black,
+  },
 
-	control: {
-		backgroundColor: theme.colors[theme.primaryColor][6],
-	},
+  control: {
+    backgroundColor: theme.colors[theme.primaryColor][6],
+  },
 }));
 
-const social = [IconBrandTwitter, IconBrandYoutube, IconBrandInstagram];
-
 const ContactUs: NextPage = () => {
-	const { classes } = useStyles();
+  const { classes } = useStyles();
 
-	const icons = social.map((Icon, index) => (
-		<ActionIcon
-			key={index}
-			size={28}
-			className={classes.social}
-			variant="transparent"
-		>
-			<Icon size={22} stroke={1.5} />
-		</ActionIcon>
-	));
+  return (
+    <Stack>
+      <SimpleGrid
+        cols={2}
+        spacing={50}
+        breakpoints={[{ maxWidth: "sm", cols: 1 }]}
+        className={classes.wrapper}
+      >
+        <div>
+          <Title className={classes.title}>Contact us</Title>
+          <Text className={classes.description} mt="sm" mb={30}>
+            Leave your email and we will get back to you within 24 hours
+          </Text>
+        </div>
+        <div className={classes.form}>
+          <TextInput
+            label="Email"
+            placeholder="your@email.com"
+            required
+            classNames={{
+              input: classes.input,
+              label: classes.inputLabel,
+            }}
+          />
+          <TextInput
+            label="Name"
+            placeholder="John Doe"
+            mt="md"
+            classNames={{
+              input: classes.input,
+              label: classes.inputLabel,
+            }}
+          />
+          <Textarea
+            required
+            label="Your message"
+            placeholder="I found a bug on..."
+            minRows={4}
+            mt="md"
+            classNames={{
+              input: classes.input,
+              label: classes.inputLabel,
+            }}
+          />
 
-	return (
-		<Stack>
-			<SimpleGrid
-				cols={2}
-				spacing={50}
-				breakpoints={[{ maxWidth: "sm", cols: 1 }]}
-				className={classes.wrapper}
-			>
-				<div>
-					<Title className={classes.title}>Contact us</Title>
-					<Text className={classes.description} mt="sm" mb={30}>
-						Leave your email and we will get back to you within 24
-						hours
-					</Text>
-
-					{/* <ContactIconsList variant="white" /> */}
-
-					<Group mt="xl">{icons}</Group>
-				</div>
-				<div className={classes.form}>
-					<TextInput
-						label="Email"
-						placeholder="your@email.com"
-						required
-						classNames={{
-							input: classes.input,
-							label: classes.inputLabel,
-						}}
-					/>
-					<TextInput
-						label="Name"
-						placeholder="John Doe"
-						mt="md"
-						classNames={{
-							input: classes.input,
-							label: classes.inputLabel,
-						}}
-					/>
-					<Textarea
-						required
-						label="Your message"
-						placeholder="I want to order your goods"
-						minRows={4}
-						mt="md"
-						classNames={{
-							input: classes.input,
-							label: classes.inputLabel,
-						}}
-					/>
-
-					<Group position="right" mt="md">
-						<Button className={classes.control}>
-							Send message
-						</Button>
-					</Group>
-				</div>
-			</SimpleGrid>
-			<Container fluid>
-				<Footer />
-			</Container>
-		</Stack>
-	);
+          <Group position="right" mt="md">
+            <Button className={classes.control}>Send message</Button>
+          </Group>
+        </div>
+      </SimpleGrid>
+      <Container fluid>
+        <Footer />
+      </Container>
+    </Stack>
+  );
 };
 
 export default ContactUs;
