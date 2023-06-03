24d7ad1cc18c19ec39daf00a00cf2fa26857db3b
Phymas <garylau@live.com>
2023-06-03 14:06:05 +0800

    UI: Add social media settings

--- src/components/Setting/AccountSetting.tsx
+++ src/components/Setting/AccountSetting.tsx
@@ -1,93 +1,291 @@
 import {
-	Stack,
-	TextInput,
-	Grid,
-	Button,
-	PasswordInput,
+  Stack,
+  TextInput,
+  Grid,
+  Button,
+  PasswordInput,
+  Avatar,
+  Title,
 } from "@mantine/core";
-import { IconTrash } from "@tabler/icons";
+import { Dropzone, IMAGE_MIME_TYPE } from "@mantine/dropzone";
+import {
+  IconBrandDribbble,
+  IconBrandFacebook,
+  IconBrandGithub,
+  IconBrandInstagram,
+  IconBrandReddit,
+  IconBrandSnapchat,
+  IconBrandTelegram,
+  IconBrandTiktok,
+  IconBrandTwitter,
+  IconBrandYoutube,
+  IconTrash,
+} from "@tabler/icons";
 import { useSession } from "next-auth/react";
 import SettingLayout from "./SettingLayout";
+import { useQuery } from "@apollo/client";
+import { GET_USER } from "@operations/queries";
+import { useState } from "react";
 
+type USER_TYPE = {
+  id: string;
+  name: string;
+  email: string;
+  image: string;
+  sex: string;
+  age: number;
+  facebookLink: string;
+  twitterLink: string;
+  instagramLink: string;
+  githubLink: string;
+  dribbbleLink: string;
+  youtubeLink: string;
+  telegramLink: string;
+  tiktokLink: string;
+  redditLink: string;
+  snapchatLink: string;
+  about: string;
+  faculty: string;
+  major: string;
+  year: number;
+  cgpa: number;
+};
 const AccountSetting = () => {
-	const { data: session } = useSession();
-	const configuration = [
-		{
-			label: "Display Name",
-			description:
-				"The name displayed on your profile and @mention by other users",
-			input: (
-				<TextInput
-					mt="-1em"
-					mb="1em"
-					type="text"
-					placeholder={session?.user.name || ""}
-				></TextInput>
-			),
-		},
-		{
-			label: "Email Address",
-			description:
-				"Update the email address associated with your account",
-			input: (
-				<TextInput
-					mt="-1em"
-					mb="1em"
-					type="email"
-					placeholder={session?.user.email || ""}
-				></TextInput>
-			),
-		},
-		{
-			label: "Password",
-			description: "Change the password used to log in to your account.",
-			input: <PasswordInput />,
-		},
-		{
-			label: "Delete Account",
-			description:
-				"Permanently delete your account and all associated data",
-			input: (
-				<Button color="red" leftIcon={<IconTrash />}>
-					Delete Account
-				</Button>
-			),
-		},
-	];
-	return (
-		<Stack>
-			<Grid>
-				<Grid.Col span={6}>
-					<SettingLayout
-						layout="vertical"
-						label={configuration[0].label}
-						description={configuration[0].description}
-						input={configuration[0].input}
-					/>
-				</Grid.Col>
-				<Grid.Col span={6}>
-					<SettingLayout
-						layout="vertical"
-						label={configuration[1].label}
-						description={configuration[1].description}
-						input={configuration[1].input}
-					/>
-				</Grid.Col>
-			</Grid>
-			<SettingLayout
-				layout="vertical"
-				label={configuration[2].label}
-				description={configuration[2].description}
-				input={configuration[2].input}
-			/>
-			<SettingLayout
-				layout="horizontal"
-				label={configuration[3].label}
-				description={configuration[3].description}
-				input={configuration[3].input}
-			/>
-		</Stack>
-	);
+  const { data: session } = useSession();
+  //   const { profileImage, setProfileImage } = useState(null);
+  const {
+    data: userData,
+    loading,
+  }: { data: USER_TYPE | undefined; loading: boolean } = useQuery(
+    GET_USER(`
+		  sex
+		  age
+		  image
+		  facebookLink
+		  twitterLink
+		  instagramLink
+		  githubLink
+		  dribbbleLink
+		  youtubeLink
+		  telegramLink
+		  tiktokLink
+		  redditLink
+		  snapchatLink
+		  about
+		  faculty
+		  major
+		  year
+		  cgpa`),
+    {
+      variables: {
+        id: session?.user.id,
+      },
+    }
+  );
+
+  const configuration: {
+    label: string | React.ReactNode;
+    description: string;
+    layout?: "horizontal" | "vertical";
+    input: JSX.Element;
+  }[] = [
+    {
+      label: "Profile Picture",
+      description: "Update your profile picture",
+      layout: "horizontal",
+      input: (
+        <Dropzone
+          accept={IMAGE_MIME_TYPE}
+          maxFiles={1}
+          onDrop={() => {}}
+          sx={{
+            position: "relative",
+            borderRadius: "999rem",
+            width: "fit-content",
+            height: "fit-content",
+          }}
+        >
+          <Avatar radius="999rem" size="xl" src={session?.user.image || null} />
+        </Dropzone>
+      ),
+    },
+    {
+      label: "Display Name",
+      description:
+        "The name displayed on your profile and @mention by other users",
+      layout: "horizontal",
+      input: (
+        <TextInput
+          mt="-1em"
+          mb="1em"
+          type="text"
+          placeholder={session?.user.name || ""}
+        ></TextInput>
+      ),
+    },
+    {
+      label: "Email Address",
+      description: "Update the email address associated with your account",
+      layout: "horizontal",
+      input: (
+        <TextInput
+          mt="-1em"
+          mb="1em"
+          type="email"
+          placeholder={session?.user.email || ""}
+        />
+      ),
+    },
+    {
+      label: <Title order={2}>Social Media</Title>,
+      description: "Update your social media links",
+      input: (
+        <Stack>
+          <SettingLayout
+            layout="horizontal"
+            label="Facebook"
+            description="Update your Facebook link"
+            input={
+              <TextInput
+                placeholder={userData?.facebookLink || ""}
+                icon={<IconBrandFacebook strokeWidth={1} />}
+              />
+            }
+          />
+          <SettingLayout
+            layout="horizontal"
+            label="Twitter"
+            description="Update your Twitter link"
+            input={
+              <TextInput
+                placeholder={userData?.twitterLink || ""}
+                icon={<IconBrandTwitter strokeWidth={1} />}
+              />
+            }
+          />
+          <SettingLayout
+            layout="horizontal"
+            label="Instagram"
+            description="Update your Instagram link"
+            input={
+              <TextInput
+                placeholder={userData?.instagramLink || ""}
+                icon={<IconBrandInstagram strokeWidth={1} />}
+              />
+            }
+          />
+          <SettingLayout
+            layout="horizontal"
+            label="Github"
+            description="Update your Github link"
+            input={
+              <TextInput
+                placeholder={userData?.githubLink || ""}
+                icon={<IconBrandGithub strokeWidth={1} />}
+              />
+            }
+          />
+          <SettingLayout
+            layout="horizontal"
+            label="Dribbble"
+            description="Update your Dribbble link"
+            input={
+              <TextInput
+                placeholder={userData?.dribbbleLink || ""}
+                icon={<IconBrandDribbble strokeWidth={1} />}
+              />
+            }
+          />
+          <SettingLayout
+            layout="horizontal"
+            label="Youtube"
+            description="Update your Youtube link"
+            input={
+              <TextInput
+                placeholder={userData?.youtubeLink || ""}
+                icon={<IconBrandYoutube strokeWidth={1} />}
+              />
+            }
+          />
+          <SettingLayout
+            layout="horizontal"
+            label="Telegram"
+            description="Update your Telegram link"
+            input={
+              <TextInput
+                placeholder={userData?.telegramLink || ""}
+                icon={<IconBrandTelegram strokeWidth={1} />}
+              />
+            }
+          />
+          <SettingLayout
+            layout="horizontal"
+            label="Tiktok"
+            description="Update your Tiktok link"
+            input={
+              <TextInput
+                placeholder={userData?.tiktokLink || ""}
+                icon={<IconBrandTiktok strokeWidth={1} />}
+              />
+            }
+          />
+          <SettingLayout
+            layout="horizontal"
+            label="Reddit"
+            description="Update your Reddit link"
+            input={
+              <TextInput
+                placeholder={userData?.redditLink || ""}
+                icon={<IconBrandReddit strokeWidth={1} />}
+              />
+            }
+          />
+          <SettingLayout
+            layout="horizontal"
+            label="Snapchat"
+            description="Update your Snapchat link"
+            input={
+              <TextInput
+                placeholder={userData?.snapchatLink || ""}
+                icon={<IconBrandSnapchat strokeWidth={1} />}
+              />
+            }
+          />
+        </Stack>
+      ),
+    },
+    {
+      label: "Password",
+      description: "Change the password used to log in to your account.",
+      input: <PasswordInput />,
+    },
+    {
+      label: "Delete Account",
+      description: "Permanently delete your account and all associated data",
+      layout: "horizontal",
+      input: (
+        <Button color="red" leftIcon={<IconTrash />}>
+          Delete Account
+        </Button>
+      ),
+    },
+  ];
+
+  console.log(userData);
+  return (
+    <Stack>
+      {configuration.map((config) => {
+        return (
+          <SettingLayout
+            layout={config.layout || "vertical"}
+            label={config.label}
+            description={config.description}
+            input={config.input}
+          />
+        );
+      })}
+    </Stack>
+  );
 };
 
 export default AccountSetting;
--- src/components/Setting/Setting.tsx
+++ src/components/Setting/Setting.tsx
@@ -1,14 +1,4 @@
-import {
-	Card,
-	Container,
-	Flex,
-	Stack,
-	Switch,
-	Tabs,
-	Text,
-	TextInput,
-} from "@mantine/core";
-import { createStyles } from "@mantine/core";
+import { Card, Container, Tabs, Text, Title } from "@mantine/core";
 import { useSession } from "next-auth/react";
 import AccountSetting from "./AccountSetting";
 import ChatSetting from "./ChatSetting";
@@ -17,95 +7,75 @@ import NotificationSetting from "./NotificationSetting";
 import SecuritySetting from "./SecuritySetting";
 
 interface Setting {
-	setting: {
-		accountSettings: SettingProps;
-		securitySettings: SettingProps;
-		notificationSettings: SettingProps;
-		communitySettings: SettingProps;
-		chatSettings: SettingProps;
-	};
+  setting: {
+    accountSettings: SettingProps;
+    securitySettings: SettingProps;
+    notificationSettings: SettingProps;
+    communitySettings: SettingProps;
+    chatSettings: SettingProps;
+  };
 }
 
 interface SettingProps {
-	value: string;
-	label: string;
-	description: string;
-	data: {
-		label: string;
-		type: string;
-		description: string;
-	}[];
+  value: string;
+  label: string;
+  description: string;
+  data: {
+    label: string;
+    type: string;
+    description: string;
+  }[];
 }
 
 const tabs = [
-	"Account",
-	"Security & Privacy",
-	"Notification",
-	"Community",
-	"Chat & Messaging",
+  "Account",
+  "Security & Privacy",
+  "Notification",
+  "Community",
+  "Chat & Messaging",
 ];
 
 const Setting: React.FC<Setting> = (props) => {
-	const { setting } = props;
-	const { data: session } = useSession();
-	const items = tabs.map((tab) => (
-		<Tabs.Tab value={tab} key={tab}>
-			{tab}
-		</Tabs.Tab>
-	));
+  const { setting } = props;
+  const items = tabs.map((tab) => (
+    <Tabs.Tab value={tab} key={tab}>
+      {tab}
+    </Tabs.Tab>
+  ));
 
-	return (
-		<>
-			<Card
-				sx={(theme) => ({
-					backgroundColor:
-						theme.colorScheme === "dark"
-							? theme.colors.dark[7]
-							: theme.white,
-				})}
-			>
-				<Tabs defaultValue="Account">
-					<Tabs.List>{items}</Tabs.List>
-					<Container p={10}>
-						{Object.entries(setting).map(([key, value]) => {
-							return (
-								<Tabs.Panel
-									key={key}
-									value={value.value}
-									pt="xs"
-								>
-									<Text size="md" weight={700}>
-										{value.label}
-									</Text>
-									<Text
-										size="xs"
-										color="dimmed"
-										mt={3}
-										mb="xl"
-									>
-										{value.description}
-									</Text>
-									{key === "accountSettings" && (
-										<AccountSetting />
-									)}
-									{key === "securitySettings" && (
-										<SecuritySetting />
-									)}
-									{key === "notificationSettings" && (
-										<NotificationSetting />
-									)}
-									{key === "communitySettings" && (
-										<CommunitySetting />
-									)}
-									{key === "chatSettings" && <ChatSetting />}
-								</Tabs.Panel>
-							);
-						})}
-					</Container>
-				</Tabs>
-			</Card>
-		</>
-	);
+  return (
+    <>
+      <Card
+        sx={(theme) => ({
+          backgroundColor:
+            theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
+        })}
+      >
+        <Tabs defaultValue="Account">
+          <Tabs.List>{items}</Tabs.List>
+          <Container p={10}>
+            {Object.entries(setting).map(([key, value]) => {
+              return (
+                <Tabs.Panel key={key} value={value.value} pt="xs">
+                  <Title order={1} size="h2">
+                    {value.label}
+                  </Title>
+                  <Text size="xs" color="dimmed" mt={3} mb="xl">
+                    {value.description}
+                  </Text>
+                  {key === "accountSettings" && <AccountSetting />}
+                  {key === "securitySettings" && <SecuritySetting />}
+                  {key === "notificationSettings" && <NotificationSetting />}
+                  {key === "communitySettings" && <CommunitySetting />}
+                  {key === "chatSettings" && <ChatSetting />}
+                </Tabs.Panel>
+              );
+            })}
+          </Container>
+        </Tabs>
+      </Card>
+    </>
+  );
 };
 
 export default Setting;
