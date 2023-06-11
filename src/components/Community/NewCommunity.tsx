57a3a971c4c721568534ed5365e6b0272a4cb280
Phymas <garylau@live.com>
2023-06-11 20:13:49 +0800

    UI: Complete the add new community ui

--- src/components/Community/NewCommunity.tsx
+++ src/components/Community/NewCommunity.tsx
@@ -1,6 +1,7 @@
 import { FormLayout } from "@components/Form";
-import { Grid } from "@mantine/core";
-
+import { Avatar, Button, Flex, Input, Space, Textarea } from "@mantine/core";
+import { Dropzone, IMAGE_MIME_TYPE } from "@mantine/dropzone";
+import { useForm } from "@mantine/form";
 type FormLayoutProps = {
   layout: "horizontal" | "vertical";
   label: string | React.ReactNode;
@@ -8,35 +9,103 @@ type FormLayoutProps = {
   input: React.ReactNode;
 };
 
-const Form: FormLayoutProps[] = [
-  {
-    layout: "horizontal",
-    label: "Name",
-    description: "Name of the community",
-    input: <></>,
-  },
-  {
-    layout: "horizontal",
-    label: "Description",
-    description: "Description of the community",
-    input: <></>,
-  },
-  {
-    layout: "horizontal",
-    label: "Avatar",
-    description: "Avatar of the community",
-    input: <></>,
-  },
-  {
-    layout: "horizontal",
-    label: "Banner",
-    description: "Banner of the community",
-    input: <></>,
-  },
-];
 const NewCommunity = () => {
+  const form = useForm({
+    initialValues: {
+      name: "",
+      description: "",
+      avatarImg: "",
+      bannerImg: "",
+    },
+  });
+
+  const Form: FormLayoutProps[] = [
+    {
+      layout: "horizontal",
+      label: "Avatar",
+      description:
+        "The avatar image is a small image that represents your community. It will be displayed next to the community name in all forum listings. Recommended image size: 100x100 pixels",
+      input: (
+        <Dropzone
+          accept={IMAGE_MIME_TYPE}
+          maxFiles={1}
+          onDrop={(img) => {
+            form.setValues({ avatarImg: URL.createObjectURL(img[0]) });
+          }}
+          sx={{
+            position: "relative",
+            borderRadius: "999rem",
+            width: "fit-content",
+            height: "fit-content",
+          }}
+        >
+          <Avatar
+            radius="999rem"
+            size="xl"
+            src={form.values.avatarImg || null}
+            onLoad={() => {
+              URL.revokeObjectURL(form.values.avatarImg);
+            }}
+          />
+        </Dropzone>
+      ),
+    },
+    {
+      layout: "horizontal",
+      label: "Banner",
+      description:
+        "The banner image is a larger image that appears at the top of your community's forum page. It can be used to showcase your community's purpose, goals, or members. Recommended image size: 1920x1200 pixels",
+      input: (
+        <Dropzone
+          accept={IMAGE_MIME_TYPE}
+          maxFiles={1}
+          onDrop={(img) => {
+            form.setValues({ bannerImg: URL.createObjectURL(img[0]) });
+          }}
+        >
+          <Avatar
+            size="xl"
+            src={form.values.bannerImg || null}
+            onLoad={() => {
+              URL.revokeObjectURL(form.values.bannerImg);
+            }}
+            sx={{
+              width: "100%",
+              height: "fit-content",
+            }}
+          />
+        </Dropzone>
+      ),
+    },
+    {
+      layout: "horizontal",
+      label: "Name",
+      description:
+        "The name of the community is what users will see when they browse forum listings. It should be clear, concise, and easy to remember.",
+      input: <Input placeholder="Name" {...form.getInputProps("name")} />,
+    },
+    {
+      layout: "horizontal",
+      label: "Description",
+      description:
+        "The description of the community is a brief overview of what your community is about. It should be informative and engaging, and it should give users a good idea of what they can expect from your community.",
+      input: (
+        <Textarea
+          placeholder="Description"
+          autosize
+          maxRows={10}
+          spellCheck
+          {...form.getInputProps("description")}
+        />
+      ),
+    },
+  ];
+
   return (
-    <form>
+    <form
+      onSubmit={form.onSubmit((values) => console.log(values))}
+      onReset={form.onReset}
+    >
       {Form.map((form, index) => (
         <FormLayout
           key={index}
@@ -46,6 +115,13 @@ const NewCommunity = () => {
           input={form.input}
         />
       ))}
+      <Space h="md" />
+      <Flex direction="row-reverse">
+        <Button type="submit">Create</Button>
+        <Button type="reset" variant="subtle" color="red">
+          Cancel
+        </Button>
+      </Flex>
     </form>
   );
 };
