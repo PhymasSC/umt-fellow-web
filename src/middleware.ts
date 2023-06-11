5d0302845e8fbc4388579c65290a58ebf5f35b4b
Phymas <garylau@live.com>
2023-06-11 15:05:40 +0800

    Feat: Restrict authorized user from double login

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,24 @@
-export { default } from "next-auth/middleware";
+import { getToken } from "next-auth/jwt";
+import { withAuth } from "next-auth/middleware";
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 
+export default async function middleware(req: NextRequest, event: NextFetchEvent) {
+    const token = await getToken({ req });
+    const isAuthenticated = !!token;
+
+    if ((req.nextUrl.pathname.startsWith('/login') || req.nextUrl.pathname.startsWith("/register")) && isAuthenticated) {
+        return NextResponse.redirect(new URL('/', req.url));
+    }
+
+    const authMiddleware = await withAuth({
+        pages: {
+            signIn: `/login`,
+        },
+    });
+
+    // @ts-expect-error
+    return authMiddleware(req, event);
+}
 export const config = {
-    matcher: ["/rtchat", "/message", "/message/:id", "/setting"]
+    matcher: ["/rtchat", "/message", "/message/:id", "/setting", "/login", "/register"]
 }
