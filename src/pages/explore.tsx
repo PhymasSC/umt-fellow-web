6876af7979609e08df471d54fe273a07f38eed60
Phymas <garylau@live.com>
2023-06-18 23:32:19 +0800

    FIx: Feeds in explore not showing

--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -8,37 +8,37 @@ import { GET_THREADS } from "@operations/queries";
 import { Container, Grid, Flex, Skeleton } from "@mantine/core";
 
 const Explore: NextPage = () => {
-	const { loading, error, data } = useQuery(GET_THREADS);
+  const { loading, error, data } = useQuery(GET_THREADS);
 
-	return (
-		<>
-			<Head>
-				<title>Explore | {APP_NAME}</title>
-			</Head>
-			<Container
-				m="xl"
-				sx={{
-					display: "flex",
-					justifyContent: "center",
-				}}
-				fluid
-			>
-				<Grid>
-					<Container>
-						<Flex mih={50} gap="md" direction="column" wrap="wrap">
-							{loading ? (
-								<Skeleton sx={(theme) => ({ width: "100%" })}>
-									Page is loading
-								</Skeleton>
-							) : (
-								<Feed feeds={data} />
-							)}
-						</Flex>
-					</Container>
-				</Grid>
-			</Container>
-		</>
-	);
+  return (
+    <>
+      <Head>
+        <title>Explore | {APP_NAME}</title>
+      </Head>
+      <Container
+        m="xl"
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+        fluid
+      >
+        <Grid>
+          <Container>
+            <Flex mih={50} gap="md" direction="column" wrap="wrap">
+              {loading ? (
+                <Skeleton sx={(theme) => ({ width: "100%" })}>
+                  Page is loading
+                </Skeleton>
+              ) : (
+                <Feed feeds={data.getThreads} />
+              )}
+            </Flex>
+          </Container>
+        </Grid>
+      </Container>
+    </>
+  );
 };
 
 export default Explore;
