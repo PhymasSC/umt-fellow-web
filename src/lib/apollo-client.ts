d49f15f7d00496ec890cf73c8e250790ca792c44
Phymas <garylau@live.com>
2023-06-18 20:29:08 +0800

    Feat: Update cache frequently

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -3,4 +3,10 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 export const client = new ApolloClient({
 	uri: process.env.APOLLO_SERVER || "http://localhost:3000/api/graphql",
 	cache: new InMemoryCache(),
+	defaultOptions: {
+		watchQuery: {
+			fetchPolicy: 'cache-and-network',
+			errorPolicy: 'all',
+		}
+	}
 });
