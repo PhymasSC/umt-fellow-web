07341e5acce5eef9a83c80762e8b8d63e7a78e29
Phymas <garylau@live.com>
2023-06-21 01:42:35 +0800

    Render: Back to CSR due to hydration problem

--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -1,23 +1,14 @@
 import { NextPage } from "next";
 import { Profile as ProfileComponent } from "@components/index";
 import { GET_THREADS_BY_AUTHOR, GET_USER } from "@operations/queries";
-import { client } from "@lib/apollo-client";
+import { useRouter } from "next/router";
+import { useQuery } from "@apollo/client";
 
 const Profile: NextPage = (props) => {
-  // @ts-expect-error
-  const { userdata, threadData } = props;
-  return (
-    <>
-      <ProfileComponent user={userdata?.getUser} threads={threadData} />
-    </>
-  );
-};
-
-export async function getServerSideProps(context: { params: { id: string } }) {
-  const id = context.params.id;
-  try {
-    const { data: userdata } = await client.query({
-      query: GET_USER(`
+  const router = useRouter();
+  const { id } = router.query;
+  const { loading: userLoading, data: userdata } = useQuery(
+    GET_USER(`
       coverImage
 			emailVerified
 			isUMTMembership
@@ -40,24 +31,53 @@ export async function getServerSideProps(context: { params: { id: string } }) {
 			cgpa
 			created_at
 			updated_at`),
+    {
       variables: { id },
-    });
-    const { data: threadData } = await client.query({
-      query: GET_THREADS_BY_AUTHOR,
+    }
+  );
+  const { loading: threadLoading, data: threadData } = useQuery(
+    GET_THREADS_BY_AUTHOR,
+    {
       variables: { authorId: id },
-    });
-    return {
-      props: {
-        userdata: userdata,
-        threadData: threadData,
-      },
-    };
-  } catch (error) {
-    console.log(error);
-  }
+    }
+  );
+  return (
+    <>
+      {userLoading || threadLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <ProfileComponent user={userdata?.getUser} threads={threadData} />
+      )}
+    </>
+  );
+};
 
-  return {
-    notFound: true,
-  };
-}
+// export async function getServerSideProps(context: { params: { id: string } }) {
+// 	const id = context.params.id;
+// 	console.log(`id: ${id}`);
+// 	try {
+// 		const { data: userdata } = await client.query({
+// 			query: GET_USER,
+// 			variables: { id },
+// 		});
+// 		const { data: threadData } = await client.query({
+// 			query: GET_THREADS_BY_AUTHOR,
+// 			variables: { authorId: id },
+// 		});
+// 		console.log("ThreadData: ", threadData);
+// 		console.log("User data: ", userdata);
+// 		return {
+// 			props: {
+// 				userdata: userdata,
+// 				threadData: threadData,
+// 			},
+// 		};
+// 	} catch (error) {
+// 		console.log(error);
+// 	}
+
+// 	return {
+// 		notFound: true,
+// 	};
+// }
 export default Profile;
